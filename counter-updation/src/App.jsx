e1900d196dbf42b3286e260d614004fd0030d0d6
user.name like : "Sujal Raj <user.email like : "sujalraj2064@gmail.com>
2024-09-28 01:01:50 +0530

    counter with a fixed count logic committed

--- counter-updation/src/App.jsx
+++ counter-updation/src/App.jsx
@@ -1,31 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const increaseValue = ()=>{
-    if(count>=20) return
-
-    else{
-      setCount(count+1);
-    }
+  const addValue =()=>{
+    if(count>=20) return;
+    setCount(count + 1);
   }
-  const decreaseValue = ()=>{
-    if(count<=0) return
 
-    else{
-      setCount(count-1);
-    }
+  const  subtractValue =()=>{
+    if(count<=0) return;
+    setCount(count - 1);
   }
 
   return (
     <>
-     <h1>Counter {count}</h1>
-     <button onClick={increaseValue}>Add value</button>
-     <button onClick={decreaseValue}>substract value</button>
+      <h1>My counter is: {count}</h1>
+      <button
+      onClick={addValue}>Increase Value</button>
+      <br />
+      <br />
+      <button
+      onClick={subtractValue}>Decrease Value</button>
     </>
   )
 }
