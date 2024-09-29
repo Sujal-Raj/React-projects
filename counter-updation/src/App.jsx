9d43393777a264e9dcc3f4904fdd42afe76fa911
user.name like : "Sujal Raj <user.email like : "sujalraj2064@gmail.com>
2024-09-29 17:21:51 +0530

    Bg changer inside counter updation due to big file size

--- counter-updation/src/App.jsx
+++ counter-updation/src/App.jsx
@@ -2,27 +2,49 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
-  const addValue =()=>{
-    if(count>=20) return;
-    setCount(count + 1);
-  }
+  // const [count, setCount] = useState(0)
 
-  const  subtractValue =()=>{
-    if(count<=0) return;
-    setCount(count - 1);
-  }
+  // const addValue =()=>{
+  //   if(count>=20) return;
+  //   setCount(count + 1);
+  // }
+
+  // const  subtractValue =()=>{
+  //   if(count<=0) return;
+  //   setCount(count - 1);
+  // }
+
+
+  const buttons = ["red","yellow","blue","green"];
+  const [color, setColor] = useState("white")
+
 
   return (
     <>
-      <h1>My counter is: {count}</h1>
+      {/* <h1>My counter is: {count}</h1>
       <button
       onClick={addValue}>Increase Value</button>
-      <br />
+      <br /> 
       <br />
       <button
-      onClick={subtractValue}>Decrease Value</button>
+      onClick={subtractValue}>Decrease Value</button> */}
+
+
+    //backgroundColorChanger Code starts here
+
+
+      {buttons.map((items,index)=>{
+        return (
+            <button
+            onClick={()=>setColor(items)}
+            >{items}</button>
+        )
+    })}
+
+      <div style={{backgroundColor:color,height:"200px",width:"100%"}}></div>
+
     </>
   )
 }
